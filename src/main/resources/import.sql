288c959f4e28c5a4685682675a6d9c84970347df
guilhermeolivmoraes-bit <guilherme.olivmoraes@gmail.com>
2026-05-30 18:32:04 -0300

    inserindo mais o bloco e ajustando suas datas e horários

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -16,5 +16,6 @@ INSERT INTO tb_participantes_atividades (participantes_id, atividades_id) VALUES
 INSERT INTO tb_participantes_atividades (participantes_id, atividades_id) VALUES (3, 2);
 INSERT INTO tb_participantes_atividades (participantes_id, atividades_id) VALUES (4, 2);
 
-INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2022-07-25T13:00:00Z', TIMESTAMP WITH TIME ZONE '2022-07-25T15:00:00Z', 1);
-INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2022-07-30T08:00:00Z', TIMESTAMP WITH TIME ZONE '2022-07-30T11:00:00Z', 2);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z', 1);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z', 2);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 2);
